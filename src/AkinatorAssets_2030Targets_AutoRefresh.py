c71405983d10d6fce9c42c5ca87b165dfd15bafe
stout461 <stout.r3@gmail.com>
2025-06-04 20:54:33 -0500

    Update AkinatorAssets_2030Targets_AutoRefresh.py
    changing port for the dns forwarding

--- src/AkinatorAssets_2030Targets_AutoRefresh.py
+++ src/AkinatorAssets_2030Targets_AutoRefresh.py
@@ -433,4 +433,4 @@ if __name__ == '__main__':
     scheduler.add_job(scheduled_watchlist_run, 'cron', hour=16, minute=30)
     scheduler.start()
 
-    app.run(host='0.0.0.0', port=8080, debug=False)
+    app.run(host='0.0.0.0', port=80, debug=False)

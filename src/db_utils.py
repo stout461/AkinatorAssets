9a47c7fa631bdd36d432daba88a47a07102c0f90
stout461 <stout.r3@gmail.com>
2025-06-24 19:17:37 -0500

    Attempting a Hotfix for the Database #3

--- src/db_utils.py
+++ src/db_utils.py
@@ -7,16 +7,12 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DB_CONFIG = {
-    "dbname": os.getenv("DB_NAME"),
-    "user": os.getenv("DB_USER"),
-    "password": os.getenv("DB_PASSWORD"),
-    "host": os.getenv("DB_HOST"),
-    "port": int(os.getenv("DB_PORT", 5432))
-}
-
 def get_connection():
-    return psycopg2.connect(**DB_CONFIG)
+    return psycopg2.connect(dbname=os.getenv("DB_NAME"),
+                            user=os.getenv("DB_USER"),
+                            password=os.getenv("DB_PASSWORD"),
+                            host=os.getenv("DB_HOST"),
+                            port=os.getenv("DB_PORT"))
 
 def insert_ticker_if_not_exists(ticker):
     conn = get_connection()
